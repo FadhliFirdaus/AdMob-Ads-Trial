99bb9cda50f24956e8ae0d75524cb626aa63bbd6
Fadhli <fadliprestigious@gmail.com>
2022-05-18 14:41:36 +0800

    first test

--- app/src/main/java/com/example/admobtest/MainActivity.java
+++ app/src/main/java/com/example/admobtest/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.admobtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, initializationStatus -> {
+
+        });
     }
 }
