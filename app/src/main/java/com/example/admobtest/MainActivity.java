6d86e745b0ef7e8d79fd4fae8c92407a4f634dd2
Fadhli <fadliprestigious@gmail.com>
2022-05-24 19:22:17 +0800

    first test

--- app/src/main/java/com/example/admobtest/MainActivity.java
+++ app/src/main/java/com/example/admobtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admobtest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,12 +20,19 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.initialization.AdapterStatus;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,25 +42,32 @@ public class MainActivity extends AppCompatActivity {
     public AdView bannerAd;
     public static String interstitialTestId =
             "ca-app-pub-3940256099942544/1033173712";
+    public static final String rewardedStringId =
+            "ca-app-pub-3940256099942544/5224354917";
+    private RewardedAd mRewardedAd;
+    private final String TAG = "MainActivity Ad";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> testDeviceIds = Arrays.asList("B2681743EA4EB0215EA7C5797E47E37B");
+        RequestConfiguration configuration =
+                new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
         MobileAds.initialize(this, initializationStatus -> {
             //check each adapter init status before making an ad
-            //
             /*
             Initialize your ad object with an Activity instance
 In the constructor for a new ad object (for example, AdView), you must pass in an object of type Context. This Context is passed on to other ad networks when using mediation. Some ad networks require a more restrictive Context that is of type Activity and may not be able to serve ads without an Activity instance. Therefore, we recommend passing in an Activity instance when initializing ad objects to ensure a consistent experience with your mediated ad networks.
-
-
              */
+
             Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
             for(String adapter : statusMap.keySet()){
                 AdapterStatus status = statusMap.get(adapter);
                 Log.i("Mobile ads SDK", String.format("Adapter : %s, Desc : %s, Latency : %d", adapter, status.getDescription(), status.getLatency()));
             }
+            MobileAds.setRequestConfiguration(configuration);
+
 
 
         });
@@ -60,6 +75,79 @@ In the constructor for a new ad object (for example, AdView), you must pass in a
         interstitialLoadAdFunction();
         setButtonOnClick();
         nativeFunction();
+        rewardedFunction();
+    }
+
+    private void rewardedFunction() {
+        Button rewardedAdButton = findViewById(R.id.rewardedButtonContent);
+        rewardedAdButton.setOnClickListener(view -> {
+       setContentView(R.layout.rewarded_ads);
+       Button openRewardedAdButton = findViewById(R.id.rewardedButton);
+       openRewardedAdButton.setOnClickListener(v -> {
+           t("button clicked 1");
+           AdRequest req = new AdRequest.Builder().build();
+           RewardedAd.load(this, rewardedStringId, req, new RewardedAdLoadCallback() {
+               @Override
+               public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                   //handles what happens when the ad fails to load
+                   super.onAdFailedToLoad(loadAdError);
+                   Log.d(TAG, loadAdError.getMessage());
+                   mRewardedAd = null;
+               }
+
+               @Override
+               public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                   //handles what happens when the ad is loaded perfectly
+                   super.onAdLoaded(rewardedAd);
+                   mRewardedAd = rewardedAd;
+                   Log.d(TAG, "Ad was loaded perfectly");
+                   mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                       @Override
+                       public void onAdClicked() {
+                           t("rewarded ad was clicked");
+                           super.onAdClicked();
+                       }
+
+                       @Override
+                       public void onAdDismissedFullScreenContent() {
+                           t("rewarded ad dismissed");
+                           super.onAdDismissedFullScreenContent();
+                           mRewardedAd = null;
+                       }
+
+                       @Override
+                       public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                           t("rewarded ad failed to show");
+                           super.onAdFailedToShowFullScreenContent(adError);
+                       }
+
+                       @Override
+                       public void onAdImpression() {
+                           t("rewarded ad recorded an impression");
+                           super.onAdImpression();
+                       }
+
+                       @Override
+                       public void onAdShowedFullScreenContent() {
+                           t("rewarded ad shown perfectly");
+                           super.onAdShowedFullScreenContent();
+                       }
+                   });
+                   if(mRewardedAd != null){
+                       Activity context = MainActivity.this;
+                       mRewardedAd.show(context, rewardItem -> {
+                           t("user received reward");
+                           int rewardAmount = rewardItem.getAmount();
+                           String rewardType = rewardItem.getType();
+
+                       });
+                   } else {
+                       t("Rewarded ad still not ready");
+                   }
+               }
+           });
+       });
+        });
     }
 
     private void nativeFunction() {
